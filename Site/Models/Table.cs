dd1fb9e88623145a0796310e6c427ca2d6c43d11
student <student@STDNT-02-1-1-K1>
2018-09-13 15:05:56 +0300

    Add project files.

---
+++ Site/Models/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Site.Models
+{
+    public class Table 
+    {
+        public string UserName { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime AutarizationDate { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
